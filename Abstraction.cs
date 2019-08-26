88c2e4de9e30a0c84ead607ed9b9c32cae8007c4
Tony Chacon <softwareintrospectre@gmail.com>
2019-08-26 06:57:12 -0400

    Create Abstraction.cs

---
+++ Abstraction.cs
@@ -0,0 +1,57 @@
+//Abstraction is the concept of classes using functionality from other classes, without needing to know how they work
+
+using System;
+
+namespace Abstraction
+{
+
+    abstract class Dragon{
+        public abstract string magic();
+    }
+
+    class RedDragon : Dragon
+    {
+        private string breath;
+
+        public RedDragon(string redDragonMagic = "fire breath")
+        {
+           breath = redDragonMagic;
+        }
+
+        public override string magic()
+        {
+            return("magic ability is: " + breath);
+        }
+    }
+
+       class PoisonDragon : Dragon
+    {
+        private string curse;
+
+        public PoisonDragon(string psnDragonMagic = "putrid curse")
+        {
+           curse = psnDragonMagic;
+        }
+
+        public override string magic()
+        {
+            return("magic ability is: " + curse);
+        }
+    }
+
+    class MagicalEffect
+    {
+
+        static void Main(string[] args)
+        {
+            Dragon redDragon = new RedDragon();
+            Dragon psnDragon = new PoisonDragon();
+
+           string redEffect = redDragon.magic();
+           string poisonEffect = psnDragon.magic();
+
+            Console.Write("Red Dragon's " + redDragon.magic() + "\n");
+            Console.Write("Poison Dragon's " + psnDragon.magic());
+        }
+    }
+}
