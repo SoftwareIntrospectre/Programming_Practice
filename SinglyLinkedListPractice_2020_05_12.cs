10d92c230162380697819934eccd3e9058fb91a9
Tony Chacon <softwareintrospectre@gmail.com>
2020-05-12 08:49:32 -0400

    Update SinglyLinkedListPractice_2020_05_12.cs
    Adding new method: appendHead()

--- SinglyLinkedListPractice_2020_05_12.cs
+++ SinglyLinkedListPractice_2020_05_12.cs
@@ -1,84 +1,95 @@
-
-    class Program
+class Program
+{
+    public static void Main(String[] args)
     {
-        public static void Main(String[] args)
-        {
-            SinglyLinkedList singlyLL = new SinglyLinkedList();
-            
-            singlyLL.appendTail(199);
-            singlyLL.appendTail(188);
-            singlyLL.appendTail(21);
+        SinglyLinkedList singlyLL = new SinglyLinkedList();
+        
+        singlyLL.appendTail(199);
+        singlyLL.appendTail(188);
+        singlyLL.appendTail(21);
+        
+        singlyLL.appendHead(7);
 
-            singlyLL.printList();
+        singlyLL.printList();
+    }
+}
+
+//wrapping nodes with this
+class SinglyLinkedList
+{
+    public Node head;
+
+    public Node tail;
+
+    public class Node
+    {
+        public int data;
+
+        public Node next;
+
+        public Node(int d)
+        {
+          //initializing node with user input
+            data = d;
+
+          //sets new node as null, because end of Linked List
+            next = null;
         }
     }
 
-    //wrapping nodes with this
-    class SinglyLinkedList
+    public void appendHead(int newData)
     {
-        public Node head;
+        Node newNode = new Node(newData);
 
-        public Node tail;
-        
-        public class Node
+         //if head does not exist, create it
+        if(head == null)
         {
-            public int data;
 
-            public Node next;
-
-            public Node(int d)
-            {
-              //initializing node with user input
-                data = d;
-               
-              //sets new node as null, because end of Linked List
-                next = null;
-            }
+        //if only 1 node: head and tail are the same node.
+            head = newNode;
+            tail = newNode;
         }
 
-        
-        public void appendTail(int newData)
-        {
-            Node newNode = new Node(newData);
+        Node newHead = new Node(newData);
+        newHead.next = head;
+        head = newHead;
 
-            //if head does not exist, create it
-            if(head == null)
-            {
-            
-            //if only 1 node: head and tail are the same node.
-                head = newNode;
-                tail = newNode;
-                
-                //exit after creating head, because everything else relies on head existing
-                return;
-            }
-            
-              //change tail's null pointer to the new node
-                tail.next = newNode;
-                
-                //changing tail of list to the new last node
-                tail = newNode;
-                return;
+        //exit after creating head, because everything else relies on head existing
+        return;
+    }
+
+    public void appendTail(int newData)
+    {
+        Node newNode = new Node(newData);
+
+        appendHead(newData);
+
+        //change tail's null pointer to the new node
+        tail.next = newNode;
+
+        //changing tail of list to the new last node
+        tail = newNode;
+        return;
+    }
+
+    public void printList()
+    {
+        if(head == null)
+        {
+            Console.WriteLine("List is empty");
+            return;
         }
 
-        public void printList()
-        {
-            if(head == null)
-            {
-                Console.WriteLine("List is empty");
-                return;
-            }
+        //initialize "current" at start of Linked List
+        Node currentNode = head;
 
-            //initialize "current" at start of Linked List
-            Node currentNode = head;
-            
-            //only last node will have a null pointer
-            while(currentNode != null)
-            {
-                Console.WriteLine(currentNode.data);
-                
-                //iterates to next node in list
-                currentNode = currentNode.next;
-            }
+        //only last node will have a null pointer
+        while(currentNode != null)
+        {
+            Console.WriteLine(currentNode.data);
+
+            //iterates to next node in list
+            currentNode = currentNode.next;
         }
     }
+}
