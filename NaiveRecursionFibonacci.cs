e7875a794de2bae454c3e993fa67776b090b79db
Tony Chacon <softwareintrospectre@gmail.com>
2020-06-01 07:08:28 -0400

    Create NaiveRecursionFibonacci.cs
    Program that practices naive recursion. Used to learn the concepts, and to illustrate how this is not an effective solution. Will build on this with memoization.

---
+++ NaiveRecursionFibonacci.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Linq;
+
+namespace ExampleCodePlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            fibonacci(5);
+        }
+
+        static int fibonacci(int n)
+        {
+            if(n == 1)
+            {
+                Console.WriteLine("n: " + fibonacci(n));
+                return 1;
+            }
+
+            else if(n == 2)
+            {
+                Console.WriteLine("n: " + fibonacci(n));
+                return 1;
+            }
+
+            else if (n > 2)
+            {
+                Console.WriteLine("n: " + fibonacci(n));
+                return fibonacci(n-1) + fibonacci(n-2);
+            }
+
+            return 0;
+        }
+    }
+
+}
